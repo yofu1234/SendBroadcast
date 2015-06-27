e1708f3ca1b2835fe1a05c5a81c1e9745e3f57a9
Yo-Yo <yofu1234@gmail.com>
2015-06-26 23:13:03 -0700

    Initial commit

---
+++ app/src/main/java/com/thenewboston/sendbroadcast/MainActivity.java
@@ -0,0 +1,46 @@
+package com.thenewboston.sendbroadcast;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendOutBroadcast(View view) {
+        Intent i = new Intent(); //create Intent
+        i.setAction("com.thenewboston.sendbroadcast"); //set keyword tied to broadcast so receiver knows which broadcast to listen for
+        i.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES); //this line make sures that the intent broadcast is compatible w/ all android versions
+        sendBroadcast(i); //sends out broadcast
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
